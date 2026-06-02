f26fddbaa8d872d8eeeb8132fae9ebe847bf0043
Tanushree Bobade <tanushreebobade@gmail.com>
2026-06-02 12:00:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (77.57%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-     //dutuch national flag algo - 3 pointer
-    int n =nums.size();
-     int low = 0 ,mid = 0, high = n-1;
-     while(mid <= high){
-        if(nums[mid] == 0){
-            swap(nums[low],nums[mid]);
-            low++;       
-            mid++;
+        // dutuch national flag algo - 3 pointer
+        int n = nums.size();
+        int low = 0, mid = 0, high = n - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
 
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else if (nums[mid] == 2) {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
         }
-        else if(nums[mid]==1){
-            mid++;
-        }
-        else if(nums[mid]==2){
-            swap(nums[mid],nums[high]);
-            high--;
-        }
-
-     } 
     }
 };
