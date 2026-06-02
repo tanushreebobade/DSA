0a9d7d0b8b81214af4b80c1546636d9a1992305b
Tanushree Bobade <tanushreebobade@gmail.com>
2026-06-02 12:55:57 +0530

    Time: 10 ms (40.93%) | Memory: 91.8 MB (85.65%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -10,9 +10,12 @@ public:
 
         // land -> water i
         for (int i = 0; i < n; i++) {
+            // water -> land j
+
             for (int j = 0; j < m; j++) {
                 int landfin = landStartTime[i] + landDuration[i];
-                int finish1 = max(landfin, waterStartTime[j]) + waterDuration[j];
+                int finish1 =
+                    max(landfin, waterStartTime[j]) + waterDuration[j];
 
                 int waterfinish = waterStartTime[j] + waterDuration[j];
                 int finish2 =
@@ -24,3 +27,6 @@ public:
         return ans;
     }
 };
+
+// Time:O(n*m)
+// Space:O(1)
