c2c1b107c5d5c4849893728a21b4bf074bb40757
Tanushree Bobade <tanushreebobade@gmail.com>
2026-06-09 10:23:57 +0530

    Time: 19 ms (53.95%) | Memory: 57.1 MB (81.07%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,20 +10,49 @@
  */
 class Solution {
 public:
+
+    ListNode* merge(ListNode* left, ListNode* right){
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        while(left && right){
+            if(left->val <= right->val){
+                tail->next = left;
+                left = left->next;
+            }
+            else{
+                tail->next = right;
+                right = right->next;
+            }
+
+            tail = tail->next;
+        }
+
+        if(left) tail->next = left;
+        if(right) tail->next = right;
+
+        return dummy.next;
+    }
+
     ListNode* sortList(ListNode* head) {
-        multiset<int> ms;
-        ListNode* temp = head;
-        while(temp!=NULL){
-            ms.insert(temp->val);
-            temp = temp ->next;
+
+        if(!head || !head->next)
+            return head;
+
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        temp = head;
-        for(auto it :ms ){
-            temp ->val = it;
-            temp =temp->next;
-        }
+        ListNode* mid = slow->next;
+        slow->next = NULL;
 
-        return head;
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(mid);
+
+        return merge(left, right);
     }
 };
