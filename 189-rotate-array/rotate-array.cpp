bde6d21bcf5775f70af49755a5414fdbc34c16fd
Tanushree Bobade <tanushreebobade@gmail.com>
2026-07-01 21:45:16 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (44.34%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
+    void reversearr(vector<int>& nums, int start, int end) {
+        while (start < end) {
+              swap(nums[start++], nums[end--]);
+        }
+    }
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        k = k% n;
-      
-            reverse(nums.begin(), nums.begin()+(n-k));
-            reverse(nums.begin()+(n-k), nums.end());
-            reverse(nums.begin(), nums.end());
-            
-           
-        
+        k = k % n;
+
+        reversearr(nums, 0, n - k - 1);
+        reversearr(nums, n - k, n - 1);
+        reversearr(nums, 0, n - 1);
     }
 };
