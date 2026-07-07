5802d95f8196d6943e777aaf0fb1fc1215334a08
Tanushree Bobade <tanushreebobade@gmail.com>
2026-07-07 18:44:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (51.66%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    // int climb( int n, vector<int>&dp){
+    //     if(n <= 2){
+    //         return n;
+    //     }
+    //     if( dp[n] != -1){
+    //         return dp[n];
+    //     }
+    //      return dp[n] = climb(n-1,dp) +climb(n-2,dp);
+    // }
+
+    int climbStairs(int n) {
+        if(n <= 2){
+            return n;
+        }
+//bottom up: tabulationn ny looking at base case of topp down app..
+        vector<int>dp(n+1,-1);
+        dp[0]= 0;
+        dp[1]= 1;
+        dp[2]= 2;
+        for(int i =3; i <=n; i++ ){
+            dp[i] = dp[i-1]+dp[i-2];
+
+        }
+         return dp[n];
+
+   
+    }
+};
