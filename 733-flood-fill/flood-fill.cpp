f3eecae44abe26cefbd34faded58dc4e8336d61a
Tanushree Bobade <tanushreebobade@gmail.com>
2026-08-02 15:47:48 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (8.21%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,30 +1,27 @@
 class Solution {
-private:
-    void dfs(vector<vector<int>> image, vector<vector<int>>& ans, int row,
-             int col, int color, int ini_color) {
+public:
+    void dfs(vector<vector<int>> image, int row, int col, int color,
+             vector<vector<int>>& ans, int starting_color) {
         int n = image.size();
         int m = image[0].size();
         ans[row][col] = color;
-       int del_row[] = {-1, 0, 1, 0};
-        int del_col[] = {0, 1, 0, -1};
+        int delrow[] = {0, -1, 0, 1};
+        int delcol[] = {-1, 0, 1, 0};
         for (int i = 0; i < 4; i++) {
-            int n_row = del_row[i] + row;
-            int n_col = del_col[i] + col;
-            if (n_row >= 0 && n_row < n && n_col >= 0 && n_col < m &&
-                ans[n_row][n_col] == ini_color && ans[n_row][n_col] != color) {
-                ans[n_row][n_col] = color;
-                dfs(image, ans, n_row, n_col, color, ini_color);
+            int nrow = row + delrow[i];
+            int ncol = col + delcol[i];
+            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                image[nrow][ncol] == starting_color && ans[nrow][ncol] != color) {
+                ans[nrow][ncol] = color;
+                dfs(image, nrow, ncol, color, ans, starting_color);
             }
         }
     }
-
-public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
                                   int color) {
         vector<vector<int>> ans = image;
-        int ini_color = image[sr][sc];
-
-        dfs(image, ans, sr, sc, color, ini_color);
+        int starting_color = image[sr][sc];
+        dfs(image, sr, sc, color, ans, starting_color);
         return ans;
     }
 };
