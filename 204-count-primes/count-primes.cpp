9bd69f780f110ace7d3db006ee138aad200248c6
Tanushree Bobade <tanushreebobade@gmail.com>
2026-08-05 14:28:03 +0530

    Time: 198 ms (43.40%) | Memory: 12.6 MB (65.08%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -3,18 +3,16 @@ public:
     int countPrimes(int n) {
         vector<bool> prime(n, true);
         int cnt = 0;
-
         for (int i = 2; i < n; i++) {
             if (prime[i]) {
                 cnt++;
-                if (1LL * i * i < n) {
+                if (1LL * i * i  < n) {
                     for (int j = i * i; j < n; j += i) {
                         prime[j] = false;
                     }
                 }
             }
         }
-
         return cnt;
     }
 };
