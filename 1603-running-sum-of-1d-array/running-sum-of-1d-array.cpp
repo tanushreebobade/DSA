a203c3c6bac0c4924f289be86d87ec2c4675a0a4
Tanushree Bobade <tanushreebobade@gmail.com>
2026-08-09 19:05:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (82.42%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        vector<int>ans;
-        int sum = nums[0];
-        ans.push_back(sum);
+    
         for(int i = 1 ; i < nums.size();i++){
-          int rsum = ans[i-1]+ nums[i];
-          ans.push_back(rsum);
+          nums[i] = nums[i-1]+ nums[i];
+         
         }
-        return ans;
+        return nums;
     }
 };
